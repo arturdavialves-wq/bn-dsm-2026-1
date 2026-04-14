aac325cb7b78c1b759b11621a7d0b516be48ec62
arturdavialves-wq <arturdavialves@gmail.com>
2026-04-14 00:12:32 +0000

    (23/03) Instalação e configuração do Prisma ORM

---
+++ back-end/src/controllers/categorias.js
@@ -0,0 +1,30 @@
+import {prisma} from '../database/client.js'
+
+const controller = {}   // Objeto vazio
+
+controller.create = async function(req, res) {
+ /*
+   Conecta-se ao banco de dados e envia uma instrução
+   de criação de um novo documento, contendo os dados
+   que chegaram dentro da seção "body" da requisição
+   ("req")
+ */
+ try {
+   await prisma.categoria.create({ data: req.body })
+
+   // Envia um código de sucesso ao front-end
+   // HTTP 201: Created
+   res.status(201).end()
+ }
+ catch(error) {
+   // Algo deu errado: exibe o erro no terminal
+   console.error(error)
+
+   // Envia o erro ao front-end, com código de erro
+   // HTTP 500: Internal Server Error
+   res.status(500).send(error)
+ }
+}
+
+
+export default controller
